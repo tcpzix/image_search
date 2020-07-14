3a8ea0b1e90a5d3a4470003083b09b6d479d1cb3
Amin <asaa47721@gmail.com>
2020-07-14 14:57:47 +0430

    organize returned result

--- server.py
+++ server.py
@@ -5,7 +5,14 @@ import pickle
 from datetime import datetime
 from PIL import Image
 from feature_extractor import FeatureExtractor
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
+from dotenv import load_dotenv
+
+
+# load .env variables
+load_dotenv()
+
+PATH = os.environ.get('IMAGES_PATH')
 
 
 app = Flask(__name__) 
@@ -16,7 +23,7 @@ features = []
 img_paths = []
 for feature_path in glob.glob("static/feature/*"):
     features.append(pickle.load(open(feature_path, 'rb')))
-    img_paths.append('static/img/' + \
+    img_paths.append(PATH + \
         os.path.splitext(os.path.basename(feature_path))[0] + '.jpg')
 
 
@@ -44,26 +51,42 @@ def index():
 
 
 
-@app.route('/imagesearch', methods=['POST'])
+@app.route('/imagesearch/', methods=['POST'])
 def search():
     """API endpoint for searching and sending back most similar images"""
-    req = request.get_json()
-    file_path = req["image_address"]
-    result_count = req["result_count"]
-    # get file name
-    dirname = os.path.dirname(__file__)
-    file = os.path.join(dirname, file_path)
-    filename = os.path.basename(file_path)
-    # open file
-    img = Image.open(file)
-    # do search
-    query = fe.extract(img)
-    dists = np.linalg.norm(features - query, axis=1)
-    ids = np.argsort(dists)[:result_count]
-    scores = [(img_paths[id], str(dists[id])) for id in ids]
-    # result
-    data = dict(scores)
-    return data
+    json_data = request.get_json()
+    image_address = json_data.get("image_address", None)
+    limit = json_data.get("limit", None)
+
+    if image_address and limit:
+        # get file name
+        dirname = os.path.dirname(__file__)
+        file = os.path.join(dirname, image_address)
+        
+        # open file
+        img = Image.open(file)
+        
+        # do search
+        query = fe.extract(img)
+        dists = np.linalg.norm(features - query, axis=1)
+        ids = np.argsort(dists)[:limit]
+        scores = [(img_paths[id], str(dists[id])) for id in ids]
+
+        result = []
+        for image_path, score in scores:
+            result.append({
+                'image_path': image_path,
+                'score': score
+            })
+
+        # result
+        # data = dict(scores)
+        return jsonify(result)
+
+    # error, fields not set
+    return {
+        'detail': 'image_address and limit fields are required'
+    }, 400
 
 
 if __name__=="__main__": 
