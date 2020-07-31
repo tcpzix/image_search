356ba2797d8dd3c4e65cbfe0e9fefbd10c9fb426
Amin <asaa47721@gmail.com>
2020-07-31 21:04:59 +0430

    train jpg, jpeg and png files. only files with ends with *.1.ext

--- train_models.py
+++ train_models.py
@@ -14,15 +14,23 @@ PATH = os.environ.get('IMAGES_PATH')
 
 fe = FeatureExtractor()
 
-for img_path in sorted(glob.glob(PATH + '*.jpg')):
-    if os.path.isfile('static/feature/' + os.path.splitext(os.path.basename(img_path))[0] + '.pkl'):
-        print(img_path, "was trained befor...")
-    else:
-        print("NEW IMAGE TRAINED:", img_path)
-        try:
-            img = Image.open(img_path)
-            feature = fe.extract(img)
-            feature_path = 'static/feature/' + os.path.splitext(os.path.basename(img_path))[0] + '.pkl'
-            pickle.dump(feature, open(feature_path, 'wb'))
-        except PIL.UnidentifiedImageError:
-            print('Cannot identify image file')
+extensions = ['*.1.jpg', '*.1.jpeg', '*.1.png']
+all_files = []
+
+for ext in extensions:
+    file_path = PATH + ext
+    print('File path:', file_path)
+    for img_path in glob.glob(file_path):
+        if os.path.isfile('static/feature/' + \
+                os.path.splitext(os.path.basename(img_path))[0] + '.pkl'):
+            print(img_path, "was trained befor...")
+        else:
+            print("NEW IMAGE TRAINED:", img_path)
+            try:
+                img = Image.open(img_path)
+                feature = fe.extract(img)
+                feature_path = 'static/feature/' + \
+                    os.path.splitext(os.path.basename(img_path))[0] + '.pkl'
+                pickle.dump(feature, open(feature_path, 'wb'))
+            except PIL.UnidentifiedImageError:
+                print('Cannot identify image file')
